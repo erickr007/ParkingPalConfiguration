94b89a951dfe07c056d789127e720334208f98ba
Eric Robertson <erickr007@gmail.com>
2017-12-12 16:45:24 -0800

    adding basic LocationController methods

---
+++ ParkingPalAPI/Controllers/LocationsController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ParkingPalAPI.Services;
+using ParkingPalAPI.Models;
+using System.Net.Http;
+
+namespace ParkingPalAPI.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/locations")]
+    public class LocationsController : Controller
+    {
+        private DataAccessService DataService;
+
+        #region Constructor
+
+        public DataController(DataAccessService service)
+        {
+            this.DataService = service;
+        }
+
+        #endregion
+
+
+        #region Get Actions
+
+        /// <summary>
+        /// Returns all existing Locations
+        /// </summary>
+        [Route("")]
+        public List<ParkingLocation> Get()
+        {
+            return new List<ParkingLocation>();
+        }
+
+        /// <summary>
+        /// Returns a Location with the specified id
+        /// </summary>
+        [Route("{id: string}")]
+        public ParkingLocation Get(string id)
+        {
+            return new ParkingLocation();
+        }
+
+        #endregion
+
+
+        #region Post Actions
+
+        [Route("add")]
+        [HttpPost]
+        public ActionResult AddLocation(ParkingLocation location)
+        {
+            return new StatusCodeResult(200);
+        }
+
+        #endregion
+
+    }
+}
